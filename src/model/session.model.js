e32b26fde67d82034caa160b40b05664d7c330e4
Kushank Narula <kushanknarula.23@gmail.com>
2026-06-12 18:56:09 +0530

    addition of refreshtoken in the schema

---
+++ src/model/session.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+const sessionSchema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:[true,"userId is required"]
+    },
+    email:{
+        type:String,
+        required:[true,"email is required"]
+    },
+    refreshToken:{
+        type:String,
+        required:[true,"refreshToken is required"]
+    },
+    ip:{
+        type:String,
+        required:[true,"ip is required"]
+    },
+    userAgent:{
+        type:String,
+        required:[true,"userAgent is required"]
+    },
+    revoked:{
+        type:Boolean,
+        default:false,
+        required:true
+    }
+},{timestamps:true})
+
+const sessionModel = mongoose.model("session",sessionSchema)
+
+
+module.exports = sessionModel
