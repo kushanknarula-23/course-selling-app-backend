1b1fc98f13242d7c38e8daf9c5600e8ea7866488
Kushank Narula <kushanknarula.23@gmail.com>
2026-06-13 22:47:25 +0530

    added the authadmin admincontroller and upload(multer middleware)

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,27 +1,29 @@
 const {Router} = require("express")
 const adminRouter = Router()
+const {upload,uploadImage}= require("../middleware/multer.middleware")
+const adminController = require("../controller/admin.controller")
 /**
  * POST /api/admin/register
  */
-adminRouter.post("/register")
+adminRouter.post("/register",adminController)
 /**
  * POST /api/user/login
  */
-adminRouter.post("login")
+adminRouter.post("login",adminController)
 /**
  * POST /api/user/publishCourse
  */
-adminRouter.post("publishCourse")
+adminRouter.post("publishCourse",upload.single("image"),uploadImage,adminController)
 
 /**
  * GET /api/admin/getCourse
  */
-adminRouter.get("/getCourse")
+adminRouter.get("/getCourse",adminController)
 
 /**
  * PUT /api/admin/updateCourse
  */
-adminRouter.put("/updateCourse")
+adminRouter.put("/updateCourse",adminController)
 
 
 
